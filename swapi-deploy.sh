ef9ba7dd277e9ec9902b16e8ac653cf3fc548bc7
lpm0073 <lpm0073@gmail.com>
2022-09-28 06:51:28 -0500

    parameterize deploy script

--- swapi-deploy.sh
+++ swapi-deploy.sh
@@ -11,5 +11,15 @@
 # see:          https://github.com/StepwiseMath/openedx_devops#v-add-more-kubernetes-admins
 #------------------------------------------------------------------------------
 
-kubectl create namespace querium-swapi --dry-run=client -o yaml | kubectl apply -f -
-kubectl apply -f "./ci/deploy/environments/dev/k8s"
+if [ $# == 1 ]; then
+    echo "deploying $1 environment to Kubernetes cluster stepwisemath-global-live in namespace querium-swapi"
+
+    # create the querium-swapi namespace if it doesn't exist
+    kubectl create namespace querium-swapi --dry-run=client -o yaml | kubectl apply -f -
+
+    # apply all Kubernetes manifests for the environment
+    kubectl apply -f "./ci/deploy/environments/$1/k8s"
+else
+    echo "Usage: ./swapi-deploy.sh ENVIRONMENT where ENVIRONMENT is prod, dev, aktiv, etcetera"
+    exit 1
+fi
