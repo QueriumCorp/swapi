02a6a28ce5a5463ef1b2bff5317692707732b8fc
Evan <igobyE@gmail.com>
2022-12-19 12:08:08 -0600

    Fixed a bug on slicing the end tag

--- plugins/cleanResponse.js
+++ plugins/cleanResponse.js
@@ -6,7 +6,7 @@ const he = require("he");
 module.exports = fp(async function (fastify, opts) {
   fastify.decorate("cleanResponse", function (dirty) {
     var resultStart = dirty.indexOf("<result>") + 8;
-    var resultEnd = dirty.indexOf("</result>") - 8;
+    var resultEnd = dirty.indexOf("</result>");
     return he.decode(dirty.slice(resultStart, resultEnd));
   });
 });
